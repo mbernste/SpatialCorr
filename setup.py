8c648ed7d50803ec655f09c7c0c44c4675116704
Matthew Bernstein <berniematt18@gmail.com>
2024-01-02 10:01:15 -0500

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
     "numpy>=1.19.5",
     "pandas>=1.2.1",
     "scipy >=1.4.1",
-    "anndata>=0.7.5"
+    "anndata>=0.7.5",
     "scikit-learn>=0.24.2",
     "matplotlib",
     "seaborn>=0.11.1",
